e8a0199fbf2a69f82c6c0ca395f8480da30bf443
Pasindu-Weerasinghe <91451835+Pasindu-Weerasinghe@users.noreply.github.com>
2022-10-25 13:15:18 +0530

    Update react file

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,6 @@ import Navbar from "./components/common/Navbar";
 import Home from "./components/pages/Home";
 import CrudAdd from "./components/cruds/CrudAdd";
 import CrudTable from "./components/cruds/CrudTable";
-import CrudListView from "./components/cruds/CrudListView";
-import CrudGridView from "./components/cruds/CrudGridView";
 import CrudDetails from "./components/cruds/CrudDetails";
 import CrudEdit from "./components/cruds/CrudEdit";
 import CrudDelete from "./components/cruds/CrudDelete";
@@ -22,8 +20,6 @@ function App() {
 					<Route exact path="/" element={<Home />} />
 					<Route exact path="/api/" element={<Home />} />
 					<Route exact path="/cruds" element={<CrudTable />} />					
-					<Route exact path="/cruds/list-view" element={<CrudListView />} />
-					<Route exact path="/cruds/grid-view" element={<CrudGridView />} />
 					<Route exact path="/cruds/new" element={<CrudAdd />} />
 					<Route exact path="/cruds/:_id" element={<CrudDetails />} />
 					<Route exact path="/cruds/:_id/edit" element={<CrudEdit />} />
